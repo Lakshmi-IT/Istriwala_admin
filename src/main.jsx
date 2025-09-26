3b1e535e6d48b2a1096d288e2ea41b968831e5af
Lakshmi-IT <laksmiit.infotech@gmail.com>
2025-09-26 17:23:45 +0530

    Update main.jsx
    PWA Update

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,19 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById("root")).render(<App />);
+// Import PWA service worker register helper
+import { registerSW } from 'virtual:pwa-register'
+
+// Register service worker
+const updateSW = registerSW({
+  onNeedRefresh() {
+    // you can show a toast/banner here asking user to refresh
+    console.log('New content available, please refresh.')
+  },
+  onOfflineReady() {
+    // you can show a message like "App ready to use offline"
+    console.log('App is ready to work offline.')
+  },
+})
+
+createRoot(document.getElementById("root")).render(<App />)
