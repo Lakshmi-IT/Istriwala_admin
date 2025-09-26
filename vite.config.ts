5af5a6d08a290bcdab557b1ada2c1a0073eb2a0e
Lakshmi-IT <laksmiit.infotech@gmail.com>
2025-09-26 17:22:58 +0530

    Update vite.config.ts
    PWA Update

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -11,8 +11,38 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      manifest: {
+        name: "ADMIN ISTRIWALA",
+        short_name: "ADMIN ISTRIWALA",
+        description: "IstriWala offers reliable doorstep pickup and delivery,ironing and hygienic services. Our trained professionals ensure your clothes are neatly pressed and delivered back to you on time — fresh, crisp, and ready to wear. Convenience, quality, and care, right at your doorstep",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
   ].filter(Boolean),
   resolve: {
     alias: {
